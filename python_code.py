359fd55d527ec6a3f054655a52fb16ed7564ba36
sai pranay <56882268+bunnysai95@users.noreply.github.com>
2025-08-11 21:21:19 +0000

    2

--- python_code.py
+++ python_code.py
@@ -1,1 +1,11 @@
-print("first try online ")
+# Calculate the multiplication and sum of two numbers
+
+input_variable =  int(input(("please enter random number 1")))
+input_variable_2 = int(input(("please enter random number 2 ")))
+
+print(input_variable+input_variable_2)
+print(input_variable*input_variable_2)
+
+# Print the Sum of a Current Number and a Previous number
+for i in range(0,10):
+    print( input_variable+input_variable_2, "and Previous number is ", input_variable)
