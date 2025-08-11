5c22cf4bfebeb28e552bdb3068eca846ca7c81f1
sai pranay <56882268+bunnysai95@users.noreply.github.com>
2025-08-11 21:21:50 +0000

    2

--- python_code.py
+++ python_code.py
@@ -9,3 +9,5 @@ print(input_variable*input_variable_2)
 # Print the Sum of a Current Number and a Previous number
 for i in range(0,10):
     print( input_variable+input_variable_2, "and Previous number is ", input_variable)
+
+print('this will be the 3rd in put that i m sharing with git ')
