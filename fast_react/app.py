5f8d0dacdda53e2835d8a9a6dca1892215b603e0
sai pranay <56882268+bunnysai95@users.noreply.github.com>
2025-08-19 01:13:58 +0000

    updated products

--- fast_react/app.py
+++ fast_react/app.py
@@ -26,6 +26,33 @@ async def get_supplier(supplier_id: int):
     response = await supplier_pydantic.from_queryset_single(Supplier.get(id=supplier_id))
     return {"status": "ok", "data":response}
 
+# this will update the supplier with id 
+@app.put('/supplier/{supplier_id}')
+async def update_supplier(supplier_id: int, update_info: supplier_pydanticIn):
+    supplier = await Supplier.get(id = supplier_id)
+    update_info = update_info.dict(exclude_unset=True)
+    supplier.name = update_info['name']
+    supplier.company = update_info['company']
+    supplier.email = update_info['email']
+    supplier.phone = update_info['phone']
+    await supplier.save()
+    response = await supplier_pydantic.from_tortoise_orm(supplier)
+    return {"status": "ok", "data": response}
+
+
+# this fuction is to delete 
+@app.delete('/supplier/{supplier_id}')
+async def delete_supplier(supplier_id: int):
+    await Supplier.filter(id=supplier_id).delete()
+    return {"status": "ok"}
+
+
+
+
+
+
+
+
 
 #  this will register the database
 register_tortoise(
