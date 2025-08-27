dde77a4eec79f33db9778c89cb1dcaef1c8d0a5a
sai pranay <56882268+bunnysai95@users.noreply.github.com>
2025-08-27 03:09:36 +0000

    8/26/25-night

--- fast_react/app.py
+++ fast_react/app.py
@@ -1,8 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from tortoise.contrib.fastapi import register_tortoise
 from models import (supplier_pydantic, supplier_pydanticIn,Supplier,product_pydantic,product_pydanticIn,Product)
 from decimal import Decimal
+from typing import List
+# email
+# from fastapi import BackgroundTasks, FastAPI
+from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
+from pydantic import BaseModel, EmailStr
+# from starlette.responses import JSONResponse
+
+
+#dotenv
+
+from dotenv import load_dotenv
+import os 
+
+# credentials
+load_dotenv()
+EMAIL = os.getenv("EMAIL")
+PASS = os.getenv("PASS")
+if not EMAIL or not PASS:
+    raise ValueError("Missing email or password environment variables")
+
+# adding cors header 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
+
+# adding cors middleware
+origins = [
+    'http://localhost:3000'
+]
+# add middleware 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )
 # this function will show the api documentation
 @app.get("/")
 def index():
@@ -46,7 +83,7 @@ async def delete_supplier(supplier_id: int):
     await Supplier.filter(id=supplier_id).delete()
     return {"status": "ok"}
 
-
+# product info 
 @app.post('/product/{supplier_id}')
 async def add_product(supplier_id: int , products_details: product_pydanticIn):
     supplier = await Supplier.get(id = supplier_id)
@@ -83,7 +120,63 @@ async def delete_product(id: int):
     await Product.filter(id=id).delete()
     return {"status": "ok", "message": f"deleted product with id {id}"}
 
-#  this will register the database
+# email configration ==================
+class EmailSchema(BaseModel):
+    email: List[EmailStr]
+
+class EmailContent(BaseModel):
+    Message: str
+    subject: str
+
+
+conf = ConnectionConfig(
+    MAIL_USERNAME = EMAIL,
+    MAIL_PASSWORD = PASS,
+    MAIL_FROM = EMAIL,
+    MAIL_PORT = 465,
+    MAIL_SERVER ="smtp.gmail.com",       # <-- correct smtp hsot for gmail
+    MAIL_STARTTLS = False,                   
+    MAIL_SSL_TLS = True,
+    USE_CREDENTIALS = True,
+    VALIDATE_CERTS = True,
+    # MAIL_FROM_NAME = "Sai Mateti" # optional 
+)
+
+@app.post("/email/{product_id}")
+async def send_email(product_id: int, content: EmailContent):
+    product = await Product.get(id = product_id)
+    supplier = await product.supplied_by
+    supplier_email = [supplier.email] if supplier and supplier.email else None
+    
+    if not supplier_email:
+        return JSONResponse(status_code=400, content={"message": "Supplier email not found."})
+
+
+    html = """
+    <p>sai mateti business LTD </p>
+    <br>
+    <p>{content.subject}</p>
+    <br>
+    <p>{content.Message}</p>
+    <br>
+    <h6> Best regards </h6>
+    <h6> Sai Mateti </h6>
+
+    """
+    message = MessageSchema(
+    subject= content.subject,
+    recipients=supplier_email,
+    body=html,
+    subtype=MessageType.html)
+
+    fm = FastMail(conf)
+    await fm.send_message(message)
+    return {"status": "ok"}
+
+             
+
+
+#  this will register the database tortoise
 register_tortoise(
     app, 
     db_url = "sqlite://database.sqlite3",
