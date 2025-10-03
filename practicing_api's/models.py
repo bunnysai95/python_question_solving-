531d6b9886ecb66cee7394965ee0a2e17179f357
sai pranay <56882268+bunnysai95@users.noreply.github.com>
2025-10-03 00:06:45 -0400

    10_3_25_1

--- practicing_api's/models.py
+++ practicing_api's/models.py
@@ -1,16 +1,35 @@
 from tortoise import fields
-from tortoise. models import Model
+from tortoise.models import Model
 
 class User(Model):
     id = fields.IntField(pk=True)
-    username = fields.CharField(20, unique=True, index=True)
+    username = fields.CharField(20, unique = True, index = True)
     first_name = fields.CharField(50)
     last_name = fields.CharField(50)
-    dob = fields.DateField(null=True)
-    phone = fields.CharField(20, null=True)
-    password_hash = fields.CharField(128)
-    created_at = fields.DatetimeField(auto_now_add=True)
-    updated_at = fields.DatetimeField(auto_now=True)
-    
-    class Meta:
-        table = "users"    
+    dob = fields.DateField(null= True)
+    phone = fields.CharField(20, null = True)
+    create_at = fields.DatetimeField(auto_now_add = True)
+    update_at = filess.DatetimeField(auto_now = True)
+
+    class meta:
+        table = "user"
+
+class Profile(Modle):
+    id = fields.IntField(pk = True)
+    user = fields.OneToOneField("models.User", related_name = "profile", on_delete = fileds.CASCADE)
+    first_name = fields.CharField(50)
+    last_name = fields.CharField(50)
+    dob = fields.DateField(50)
+    gender = fields.charField(20, null= True)
+    address = fields.TextField()
+    pincode = fields.charField()
+    country = fields.cahtField(20)
+    about_me = fields.TextField()
+    file_path = fields.CahrtField(255, null = True)
+
+    create_at = fields.DatetimeField(auto_now_add = True)
+    updated_at = fields.DatetimeField(auto_now = True)
+
+    class meta:
+        table = "profiles"
+
